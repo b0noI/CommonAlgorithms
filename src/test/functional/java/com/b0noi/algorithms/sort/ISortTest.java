fa102a09686e368d05c7d43bf06ec4b07072092b
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-23 19:57:04 +0000

    MergeSort

--- src/test/functional/java/com/b0noi/algorithms/sort/ISortTest.java
+++ src/test/functional/java/com/b0noi/algorithms/sort/ISortTest.java
@@ -1,16 +1,9 @@
 package com.b0noi.algorithms.sort;
 
-import com.b0noi.algorithms.graph.unions.QuickFind;
-import com.b0noi.algorithms.graph.unions.QuickUnion;
-import com.b0noi.algorithms.graph.unions.WeightedQuickUnion;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.*;
 
 import static org.testng.Assert.*;
 
@@ -19,8 +12,10 @@ public class ISortTest {
     @DataProvider(name = "isort_implementation_provider")
     private static Object[][] isortImplementationProvider() {
         return new Object[][]{
-                {new BubbleSort(), 10_000},
+                {new SelectionSort(), 10_000},
                 {new InsertionSort(), 10_000},
+                {new ShellSort(),     30_000},
+                {new MergeSort(),     30_000},
         } ;
     }
 
@@ -49,6 +44,77 @@ public class ISortTest {
         assertEquals(inputList, expectedResult);
 
         // mocks verify
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Object[][] testInstances = ISortTest.isortImplementationProvider();
+        final List<Thread> threads = new ArrayList<>(testInstances.length);
+        for (Object[] test : testInstances) {
+            final Thread testThread = new TestSort((ISort)test[0]);
+            threads.add(testThread);
+            testThread.start();
+        }
+        do {
+            threads.forEach(System.out::println);
+            System.out.println("~~~~~~~~~~~~~~~~~~~~");
+            Thread.sleep(1000);
+        } while (threads.stream().filter(Thread::isAlive).count() > 0);
+    }
+
+}
+
+class TestSort extends Thread {
+
+    private final Map<Test, Long> testResult = new HashMap<>();
+
+    private final ISort testSortInstance;
+
+    TestSort(final ISort testSortInstance) {
+        this.testSortInstance = testSortInstance;
+    }
+
+    @Override
+    public void run() {
+        final Random random = new Random();
+        for (Test test : Test.values()) {
+            final List<Integer> inputList = new ArrayList<>(test.getTestListSize());
+            random.ints(test.getTestListSize()).forEach(inputList::add);
+            final long beforeTest = System.nanoTime();
+            testSortInstance.sort(inputList);
+            final long afterTest = System.nanoTime();
+            testResult.put(test, (afterTest - beforeTest) / 1000_000_000);
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder result = new StringBuilder();
+        result.append(String.format("Test results for: %s \n", testSortInstance.getClass()));
+        for (Test test : Test.values()) {
+            if (testResult.containsKey(test)) {
+                result.append(String.format("Result for test: %s => %d \n", test, testResult.get(test)));
+            } else {
+                result.append(String.format("Result for test: %s => STILL IN PROGRESS \n", test));
+            }
+        }
+        return result.toString();
+    }
+
+    private enum Test {
+        EASY(10_000),
+        MEDIUM(100_000),
+        HARD(1_000_000);
+
+        private final int testListSize;
+
+        Test(int testListSize) {
+            this.testListSize = testListSize;
+        }
+
+        public int getTestListSize() {
+            return testListSize;
+        }
+
     }
 
 }
