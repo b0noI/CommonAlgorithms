23121ba9d70b6f774da94246b8b38cfa98589a83
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-23 15:13:40 +0000

    refactoring naming to more correct one

--- src/main/java/com/b0noi/algorithms/sort/InsertionSort.java
+++ src/main/java/com/b0noi/algorithms/sort/InsertionSort.java
@@ -1,27 +1,24 @@
 package com.b0noi.algorithms.sort;
 
+import java.util.Collection;
 import java.util.List;
+
 
 public class InsertionSort extends AbstractSort {
 
     @Override
     public <T extends Comparable<T>> void sort(final List<T> elements) {
         for (int i = 0; i < elements.size(); i++) {
-            final int minIndex = InsertionSort.minimumIndex(i, elements);
-            if (minIndex != i) {
-                AbstractSort.swap(elements, i, minIndex);
-            }
-        }
-    }
+            for (int j = i; j > 0; j--) {
 
-    private static <T extends Comparable<T>>int minimumIndex(final int from, final List<T> elements) {
-        int minElementIndex = from;
-        for (int i = from + 1; i < elements.size(); i++) {
-            if (AbstractSort.lower(elements.get(i), elements.get(minElementIndex))) {
-                minElementIndex = i;
+                if (AbstractSort.lower(elements.get(j), elements.get(j - 1))) {
+                    AbstractSort.swap(elements, j - 1, j);
+                } else {
+                    break;
+                }
+
             }
         }
-        return minElementIndex;
     }
 
 }
---
+++ src/main/java/com/b0noi/algorithms/sort/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.b0noi.algorithms.sort;
+
+import java.util.List;
+
+public class SelectionSort extends AbstractSort {
+
+    @Override
+    public <T extends Comparable<T>> void sort(final List<T> elements) {
+        for (int i = 0; i < elements.size(); i++) {
+            final int minIndex = SelectionSort.minimumIndex(i, elements);
+            if (minIndex != i) {
+                AbstractSort.swap(elements, i, minIndex);
+            }
+        }
+    }
+
+    private static <T extends Comparable<T>>int minimumIndex(final int from, final List<T> elements) {
+        int minElementIndex = from;
+        for (int i = from + 1; i < elements.size(); i++) {
+            if (AbstractSort.lower(elements.get(i), elements.get(minElementIndex))) {
+                minElementIndex = i;
+            }
+        }
+        return minElementIndex;
+    }
+
+}
